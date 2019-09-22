50f7a51e0990ec0bbbf2a12d2e16a14ff11fc462
OpticalSpring <cosmos5477@gmail.com>
2019-09-22 23:28:53 +0900

    적 타입 확인 가능
    시간정지했을때

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    GameManager gameManager;
+    public int colorType;
+    public GameObject colorObj;
     public float normalSpeed;
     public float runSpeed;
     public float rotateSpeed;
@@ -20,7 +23,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -44,5 +47,28 @@ public class Enemy : MonoBehaviour
         gameObject.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material.color = new Vector4(1, 0, 0, 1);
         gameObject.transform.GetChild(0).parent = null;
         Destroy(gameObject);
+    }
+
+    protected void TimeStopState()
+    {
+        if(gameManager.timeStopValue>0)
+        {
+            switch (colorType)
+            {
+                case 0:
+                    colorObj.GetComponent<MeshRenderer>().material.color = new Vector4(1, 0, 0, 1);
+                    break;
+                case 1:
+                    colorObj.GetComponent<MeshRenderer>().material.color = new Vector4(0, 0, 1, 1);
+                    break;
+                case 2:
+                    colorObj.GetComponent<MeshRenderer>().material.color = new Vector4(0, 1, 0, 1);
+                    break;
+            }
+        }
+        else
+        {
+            colorObj.GetComponent<MeshRenderer>().material.color = new Vector4(1, 1, 1, 1);
+        }
     }
 }
