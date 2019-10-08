88bda8a60f080032cfcf7e1b6a3833a1c35ac0e9
OpticalSpring <54758407+OpticalSpring@users.noreply.github.com>
2019-10-08 11:50:15 +0900

    오늘의작업

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -10,6 +10,7 @@ public class UIManager : MonoBehaviour
     public Image timeUI;
     public Text[] timeText;
     public Image enemyType;
+    public GameObject weaponType;
     // Start is called before the first frame update
     void Start()
     {
@@ -39,22 +40,35 @@ public class UIManager : MonoBehaviour
         timeText[1].text = string.Format("{0:D2} : {1:D2} : {2:D2} : {3:D2}", gameManager.playTime_a, gameManager.playTime_b, gameManager.playTime_c, gameManager.playTime_d);
         if (playerState.weapon != null)
         {
+            weaponType.SetActive(true);
             switch (playerState.weapon.GetComponent<Weapon>().type)
             {
                 case 0:
                     enemyType.color = new Color(1, 0, 0, 1);
+                    weaponType.transform.GetChild(0).gameObject.SetActive(true);
+                    weaponType.transform.GetChild(1).gameObject.SetActive(false);
+                    weaponType.transform.GetChild(2).gameObject.SetActive(false);
                     break;
                 case 1:
                     enemyType.color = new Color(0, 1, 0, 1);
+                    weaponType.transform.GetChild(0).gameObject.SetActive(false);
+                    weaponType.transform.GetChild(1).gameObject.SetActive(true);
+                    weaponType.transform.GetChild(2).gameObject.SetActive(false);
                     break;
                 case 2:
                     enemyType.color = new Color(0, 0, 1, 1);
+                    weaponType.transform.GetChild(0).gameObject.SetActive(false);
+                    weaponType.transform.GetChild(1).gameObject.SetActive(false);
+                    weaponType.transform.GetChild(2).gameObject.SetActive(true);
                     break;
             }
         }
         else
         {
             enemyType.color = new Color(1, 1, 1, 1);
+            weaponType.SetActive(false);
         }
     }
+
+
 }
--- Assets/Scripts/Player/PlayerCharacterControl.cs
+++ Assets/Scripts/Player/PlayerCharacterControl.cs
@@ -10,6 +10,7 @@ public class PlayerCharacterControl : MonoBehaviour
     GameManager gameManager;
     PlayerState playerState;
     CapsuleCollider playerCap;
+    PlayerAniControl playerAni;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,7 @@ public class PlayerCharacterControl : MonoBehaviour
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         playerState = GetComponent<PlayerState>();
         playerCap = GetComponent<CapsuleCollider>();
+        playerAni = GetComponent<PlayerAniControl>();
     }
 
     // Update is called once per frame
@@ -43,7 +45,7 @@ public class PlayerCharacterControl : MonoBehaviour
 
     void TimeResum()
     {
-        if(gameManager.timeStopValue > 0 && gameManager.timeStopValue < 14.5f)
+        if (gameManager.timeStopValue > 0 && gameManager.timeStopValue < 14.5f)
         {
             if (Input.GetKeyUp(KeyCode.F))
             {
@@ -76,24 +78,39 @@ public class PlayerCharacterControl : MonoBehaviour
                     }
                     other.transform.parent = playerState.weaponPoint;
                     other.transform.localPosition = new Vector3(0, 0, 0);
-                
+                    other.transform.localEulerAngles = new Vector3(0, 0, 0);
+
+
                     gameManager.TimeStop();
                 }
             }
         }
+    }
+    public void AniShot()
+    {
+        playerState.weapon.transform.parent = null;
+        playerState.weapon.transform.LookAt(playerState.weaponTargetPos);
+        playerState.weapon.GetComponent<Weapon>().state = 2;
+        playerState.weapon.GetComponent<Rigidbody>().isKinematic = false;
+        playerState.weapon.GetComponent<Rigidbody>().AddForce(playerState.weapon.transform.forward * playerState.shotPower, ForceMode.Impulse);
+        playerState.weapon = null;
+    }
+
+    IEnumerator DelayShot()
+    {
+        yield return new WaitForSeconds(0.2f);
+        AniShot();
+
     }
 
     void Shot()
     {
         if (playerState.state == PlayerState.State.Aim && Input.GetMouseButtonUp(0))
         {
-            playerState.weapon.transform.parent = null;
-            playerState.weapon.transform.LookAt(playerState.weaponTargetPos);
-            playerState.weapon.GetComponent<Weapon>().state = 2;
-            playerState.weapon.GetComponent<Rigidbody>().isKinematic = false;
-            playerState.weapon.GetComponent<Rigidbody>().AddForce(playerState.weapon.transform.forward * playerState.shotPower, ForceMode.Impulse);
-            playerState.weapon = null;
-        }else if (Input.GetMouseButtonUp(0))
+            playerAni.aniState = 3;
+            StartCoroutine("DelayShot");
+        }
+        else if (Input.GetMouseButtonUp(0))
         {
 
             Collider[] colliderHits = Physics.OverlapSphere(playerState.weaponPoint.position, 3);
@@ -118,22 +135,24 @@ public class PlayerCharacterControl : MonoBehaviour
         float dx = target.x - obj.transform.position.x;
 
         float rotateDegree = Mathf.Atan2(dx, dz) * Mathf.Rad2Deg;
-        
-            obj.transform.rotation = Quaternion.RotateTowards(obj.transform.rotation, Quaternion.Euler(0, rotateDegree, 0), speed * Time.deltaTime);
-        
+
+        obj.transform.rotation = Quaternion.RotateTowards(obj.transform.rotation, Quaternion.Euler(0, rotateDegree, 0), speed * Time.deltaTime);
+
     }
 
     void Move()
     {
-        
+
         inputValue.x = Input.GetAxis("Horizontal");
         inputValue.y = Input.GetAxis("Vertical");
-            mainCam.transform.position = gameObject.transform.position + new Vector3(0, 2, 0);
-        
+        mainCam.transform.position = gameObject.transform.position + new Vector3(0, 2, 0);
+        playerAni.movement = 0;
+        playerAni.aniState = 0;
         if (Input.GetKey(KeyCode.LeftControl))
-            {
-                playerState.state = PlayerState.State.Sit;
-            }
+        {
+            playerAni.aniState = 1;
+            playerState.state = PlayerState.State.Sit;
+        }
         else if (playerState.state == PlayerState.State.Idle)
         {
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
@@ -144,18 +163,23 @@ public class PlayerCharacterControl : MonoBehaviour
                 mainCam.transform.GetChild(1).transform.position = targetY;
                 targetPos = mainCam.transform.GetChild(1).transform.position;
 
-                if (Input.GetKey(KeyCode.LeftShift)) {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    playerAni.movement = 2;
                     // gameObject.transform.Translate(Vector3.forward * playerState.runSpeed * Time.deltaTime);
                     gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPos, playerState.runSpeed * Time.deltaTime);
                 }
                 else
                 {
-               // gameObject.transform.Translate(Vector3.forward * playerState.normalSpeed * Time.deltaTime);
+                    playerAni.movement = 1;
+                    // gameObject.transform.Translate(Vector3.forward * playerState.normalSpeed * Time.deltaTime);
                     gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPos, playerState.normalSpeed * Time.deltaTime);
                 }
             }
-        }else if(playerState.state == PlayerState.State.Aim)
+        }
+        else if (playerState.state == PlayerState.State.Aim)
         {
+            playerAni.aniState = 2;
             mainCam.transform.GetChild(1).transform.localPosition = new Vector3(0, 0, 10);
             Vector3 targetY = mainCam.transform.GetChild(1).transform.position;
             targetY.y = gameObject.transform.position.y;
@@ -165,7 +189,7 @@ public class PlayerCharacterControl : MonoBehaviour
             //gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPos, playerState.aimSpeed * Time.deltaTime);
             gameObject.transform.Translate(new Vector3(-inputValue.x * playerState.aimSpeed * Time.deltaTime, 0, -inputValue.y * playerState.aimSpeed * Time.deltaTime));
         }
-        else if(playerState.state == PlayerState.State.Sit)
+        else if (playerState.state == PlayerState.State.Sit)
         {
             playerState.state = PlayerState.State.Idle;
         }
