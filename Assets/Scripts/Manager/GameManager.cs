75abeafb580f539f5d13d2ec55bc9e460984d135
OpticalSpring <cosmos5477@gmail.com>
2019-10-12 20:03:08 +0900

    UI작업

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,22 +9,76 @@ public class GameManager : MonoBehaviour
     public int playTime_a, playTime_b, playTime_c, playTime_d;
     float playTime;
     PauseManager pauseManager;
+    public bool gameStart;
+
+    public GameObject[] startingUI;
+    public GameObject player;
+    public GameObject mainCam;
+    public Vector3 cameraStartingPoint; public bool gameOver;
+    public bool gameClear;
+    public GameObject gameOverUI;
+    public GameObject gameClearUI;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         pauseManager = GetComponent<PauseManager>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        TimeStopDecrease();
-        TimeFlow();
+        if (gameClear == false && gameOver == false)
+        {
+            TimeStopDecrease();
+            TimeFlow();
+            GameStateCheck();
+            if (gameStart == false && Input.GetKeyDown(KeyCode.Space))
+            {
+                gameStart = true;
+                GameStart();
+            }
+        }
+        else
+        {
+            ResultSelect();
+        }
     }
-    
 
+    void GameStateCheck()
+    {
+        if (gameOver == false && player == null)
+        {
+            gameOver = true;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            StartCoroutine("GameOverDelay");
+        }
+    }
+
+    void ResultSelect()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadSceneAsync(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 1;
+
+            SceneManager.LoadSceneAsync(0);
+        }
+    }
+
+
+    IEnumerator GameOverDelay()
+    {
+        yield return new WaitForSeconds(5f);
+        gameOverUI.SetActive(true);
+        Time.timeScale = 0;
+    }
     void TimeStopDecrease()
     {
         if (pauseManager.pause == false)
@@ -41,10 +96,10 @@ public class GameManager : MonoBehaviour
 
     void TimeFlow()
     {
-        playTime += Time.deltaTime*100;
+        playTime += Time.deltaTime * 100;
         playTime_d += (int)playTime;
         playTime -= (int)playTime;
-        if(playTime_d >= 100)
+        if (playTime_d >= 100)
         {
             playTime_d = 0;
             playTime_c++;
@@ -60,14 +115,33 @@ public class GameManager : MonoBehaviour
             playTime_a++;
         }
     }
-    
-  public void TimeStop()
+
+    public void TimeStop()
     {
         Time.timeScale = 0;
         timeStopValue = 15;
         Debug.Log("TimeStop");
     }
-    
 
 
+    void GameStart()
+    {
+        startingUI[0].SetActive(false);
+        startingUI[1].SetActive(true);
+        StartCoroutine("CameraSet");
+    }
+
+    IEnumerator CameraSet()
+    {
+        for (int i = 0; i < 300; i++)
+        {
+            mainCam.transform.position = Vector3.Lerp(mainCam.transform.position, cameraStartingPoint, Time.deltaTime);
+            Debug.Log(mainCam.transform.localEulerAngles);
+            mainCam.transform.localEulerAngles = Vector3.Lerp(mainCam.transform.localEulerAngles, new Vector3(0, 360, 0), Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+        }
+        player.GetComponent<PlayerCameraControl>().enabled = true;
+        player.GetComponent<PlayerCharacterControl>().enabled = true;
+
+    }
 }
